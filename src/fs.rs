ef18ae4f8d9e862c950447797caf5fcc07a316f1
Nazım Can Altınova <canaltinova@gmail.com>
2020-05-10 21:35:45 +0200

    Run either single tarfile or pulled docker images

--- src/fs.rs
+++ src/fs.rs
@@ -15,8 +15,8 @@ pub struct FileSystem {
 impl FileSystem {
   pub fn new(args: &clap::ArgMatches, container_id: String) -> Self {
     ensure_container_folder_exists(&container_id);
-    let fs_path = args.value_of("image").unwrap();
-    let path = untar(fs_path, &container_id);
+    let image = args.value_of("image").unwrap();
+    let path = untar(image, &container_id);
 
     FileSystem { container_id, path }
   }
@@ -29,19 +29,47 @@ impl Drop for FileSystem {
   }
 }
 
-fn untar(fs_tarball: &str, container_id: &str) -> PathBuf {
-  let now = Instant::now();
-  let fs_tar_path = PathBuf::from(fs_tarball).canonicalize().unwrap();
-  info!("Starting to unpack: {:?}", fs_tar_path);
-  let file = File::open(fs_tar_path).unwrap();
+/// Either untar a single tarball or multiple layers of tarballs
+fn untar(image: &str, container_id: &str) -> PathBuf {
   let mut file_system_path = PathBuf::from(FILE_SYSTEM_ROOT);
   file_system_path.push(container_id);
 
-  let mut archive = Archive::new(GzDecoder::new(&file));
-  info!("Unpacking tar {:?} to {:?}", file, file_system_path);
-  archive.unpack(&file_system_path).unwrap();
-  info!("Unpacked the file system tar ball in {:.2?}", now.elapsed());
+  if image.find(".tar").is_some() {
+    untar_single(&image, &file_system_path, true);
+  } else {
+    let mut image_path = PathBuf::from(FILE_SYSTEM_ROOT);
+    image_path.push("images");
+    image_path.push(&image.replace("/", "_"));
+
+    info!("{:?}", image_path);
+    if image_path.exists() && image_path.is_dir() {
+      for entry in fs::read_dir(image_path).unwrap() {
+        let entry = entry.unwrap();
+        untar_single(entry.path().to_str().unwrap(), &file_system_path, false);
+      }
+    } else {
+      error!("Image could not found!");
+    }
+  }
+
   file_system_path
+}
+
+/// Untar single tarball.
+fn untar_single(image: &str, file_system_path: &PathBuf, gzipped: bool) {
+  let now = Instant::now();
+  let fs_tar_path = PathBuf::from(image).canonicalize().unwrap();
+  let file = File::open(fs_tar_path).unwrap();
+
+  info!("Unpacking tar {:?} to {:?}", file, file_system_path);
+  if gzipped {
+    let mut archive = Archive::new(GzDecoder::new(&file));
+    archive.unpack(&file_system_path).unwrap();
+  } else {
+    let mut archive = Archive::new(&file);
+    archive.unpack(&file_system_path).unwrap();
+  }
+  info!("Unpacked the file system tar ball in {:.2?}", now.elapsed());
 }
 
 fn ensure_container_folder_exists(container_id: &str) {
